cde88c57d82c3e2368f2fdc89e690b0f2fc680a8
Pavel Mineev <pavel@mineev.me>
2021-04-26 21:17:55 +0300

    add dark theme to lang-badge

--- src/components/LangBadge.tsx
+++ src/components/LangBadge.tsx
@@ -2,7 +2,7 @@ type Props = { lang: string }
 
 export function LangBadge({ lang }: Props) {
 	return (
-		<span className="inline-flex items-center mr-2 uppercase font-medium text-base bg-gray-200 text-gray-500 px-2 rounded">
+		<span className="inline-flex items-center mr-2 uppercase font-medium text-base text-gray-600 bg-gray-200 dark:bg-gray-400 dark:text-gray-900 px-2 rounded ">
 			{lang}
 		</span>
 	)
