6ac4b001f82f80966778febc870e1783ea4866e5
Pavel Mineev <pavel@mineev.me>
2021-04-30 23:46:42 +0300

    add favicons and social image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,19 +6,21 @@ export default class MyDocument extends Document {
 			<Html lang="en">
 				<Head>
 					<link href="/favicon.svg" rel="icon" type="image/svg+xml" />
-					<link href="/touch-icon.png" rel="apple-touch-icon" />
-					<link href="/favicon.svg" rel="mask-icon" color="#333333" />
+					<link href="/favicon-mask.svg" rel="mask-icon" color="##ce6fea" />
+					<link href="/touch-180x180.png" rel="apple-touch-icon" />
 					<link href="/manifest.json" rel="manifest" />
-					<meta content="#333333" name="theme-color" />
+					<meta content="##ce6fea" name="theme-color" />
 				</Head>
 				<body className="preload-transitions bg-white text-gray-800 dark:text-gray-200 dark:bg-warmGray-900 min-w-[20rem] antialiased">
 					<Main />
 					<NextScript />
-					<script
-						defer
-						src="https://static.cloudflareinsights.com/beacon.min.js"
-						data-cf-beacon='{"token": "76d7c53c33994ba987887d0298985b3c"}'
-					/>
+					{process.env.NODE_ENV === 'production' && (
+						<script
+							defer
+							src="https://static.cloudflareinsights.com/beacon.min.js"
+							data-cf-beacon='{"token": "76d7c53c33994ba987887d0298985b3c"}'
+						/>
+					)}
 				</body>
 			</Html>
 		)
