49a4c852a35b21094945b0a1fe267cc8d9b90361
Pavel Mineev <pavel@mineev.me>
2021-06-07 13:23:47 +0300

    create screenshot of a page

---
+++ src/components/SharingImage.tsx
@@ -0,0 +1,22 @@
+export function SharingImage() {
+	return (
+		<html>
+			<head></head>
+			<body>
+				<style>{`
+					body {
+						font-family: 'Inter', sans-serif;
+						color: #000;
+						background: #ccc;
+						min-height: 100vh;
+					}
+					h1 {
+						font-weight: 900;
+						font-size: 100px;
+					}
+				`}</style>
+				<h1>Hello!</h1>
+			</body>
+		</html>
+	)
+}
---
+++ src/pages/api/sharing-image.ts
@@ -0,0 +1,29 @@
+import chromium from 'chrome-aws-lambda'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import render from 'preact-render-to-string'
+
+import { SharingImage } from 'components/SharingImage'
+
+export default async function sharingImage(_: NextApiRequest, res: NextApiResponse) {
+	const browser = await chromium.puppeteer.launch({
+		args: [...(chromium.args || []), '--font-render-hinting=none', '--force-color-profile=srgb'],
+		executablePath: await chromium.executablePath,
+		headless: true,
+	})
+	const page = await browser.newPage()
+	const image = render(SharingImage())
+
+	await chromium.font('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900')
+	await chromium.font(
+		'https://raw.githack.com/googlei18n/noto-emoji/master/fonts/NotoColorEmoji.ttf'
+	)
+	await page.setViewport({ width: 1200, height: 630 })
+	await page.setContent(image)
+
+	const data = await page.screenshot({ type: 'png' })
+
+	browser.close()
+	res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
+	res.setHeader('Content-Type', 'image/png')
+	res.end(data)
+}
