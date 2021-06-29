0fa0fb9e506ef3c39c271e544673ed7a9c85ac59
Pavel Mineev <pavel@mineev.me>
2021-06-29 14:21:40 +0300

    remove dependencies from repo

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -4,6 +4,13 @@ export default {
 		sitemap: true,
 		site: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://pavel.mineev.me',
 	},
+	markdownOptions: {
+		remarkPlugins: [
+			// import('remark-slug'),
+			import('remark-code-titles'),
+			// import('remark-autolink-headings')
+		]
+	},
 	devOptions: {
 		tailwindConfig: './tailwind.config.js',
 	},
