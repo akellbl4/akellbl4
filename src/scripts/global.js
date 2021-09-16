a29d763ee7cd860c7ebc8a4822f47df55a8e5bcb
Pavel Mineev <pavel@mineev.me>
2021-09-16 13:53:21 +0300

    rename disable transitions class

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -1,11 +1,11 @@
 /**  Enable animations */
-document.body.classList.remove('preload-transitions')
+document.body.classList.remove('disable-transitions')
 
 /** Run views counters if they are on a page */
 const viewsElements = document.querySelectorAll('[data-views-for]')
 
 viewsElements.forEach((e) => {
-	fetch(`/api/views/${e.dataset.viewsFor}${e.dataset.viewsCount === '' ? '?count=1' : ''}`)
+	fetch(`/api/views/${e.dataset.viewsFor}`)
 		.then((r) => r.json())
 		.then((data) => {
 			e.textContent = `${data[e.dataset.viewsFor].replace(/\B(?=(\d{3})+(?!\d))/g, ',')} views`
