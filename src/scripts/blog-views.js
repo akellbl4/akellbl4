296fc0220f88a4fc00cf9632ccc8bc9995e26fc1
Pavel Mineev <pavel@mineev.me>
2021-09-21 11:17:58 +0300

    rewrite views script

--- src/scripts/blog-views.js
+++ src/scripts/blog-views.js
@@ -1,19 +1,16 @@
-/** Run views counters if they are on a page */
-
+/** Get list of posts and put their views to DOM */
 fetch('/blog/?views')
 	.then((res) => res.json())
-	.then((data) => {
-		console.log(data)
-	})
+	.then(setViewsToPosts)
 	.catch((err) => {
 		console.error(err)
 	})
-// const viewsElements = document.querySelectorAll('[data-views-for]')
 
-// viewsElements.forEach((e) => {
-// 	// fetch(`/api/views/${e.dataset.viewsFor}`)
-// 		// .then((r) => r.json())
-// 		// .then((data) => {
-// 		// 	e.textContent = `${data[e.dataset.viewsFor].replace(/\B(?=(\d{3})+(?!\d))/g, ',')} views`
-// 		// })
-// })
+function setViewsToPosts(posts) {
+	posts.forEach((post) => {
+		const element = document.querySelector(`[data-views-for="${post.slug}"]`)
+		const content = `${post.views} view${post.views > 1 ? 's' : ''}`
+
+		element.textContent = content
+	})
+}
