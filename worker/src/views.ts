3e51203d7674fca1e7ebe071f90618a0db933774
Paul Mineev <paul@mineev.me>
2022-08-25 14:18:33 -0700

    fix prettier and format codebase

--- worker/src/views.ts
+++ worker/src/views.ts
@@ -5,36 +5,38 @@ import { getViews, setViews } from './lib/views'
 export const router = Router<Request, IHTTPMethods>({ base: '/api/views' })
 
 router.get('/:slug', async (req: Request, env: Env): Promise<Response> => {
-  const { slug } = req.params ?? {}
+	const { slug } = req.params ?? {}
 
-  if (!slug) {
-    return new Response('Missing slug', { status: 400 })
-  }
+	if (!slug) {
+		return new Response('Missing slug', { status: 400 })
+	}
 
-  const count = await getViews(env.kv, slug)
+	const count = await getViews(env.kv, slug)
 
-  return new Response(`${count}`, { status: 200 })
+	return new Response(`${count}`, { status: 200 })
 })
-
-
 
 router.post('/:slug', async (req: Request, env: Env): Promise<Response> => {
-  const { slug } = req.params ?? {}
+	const { slug } = req.params ?? {}
 
-  if (!slug) {
-    return new Response('Missing slug', { status: 400 })
-  }
+	if (!slug) {
+		return new Response('Missing slug', { status: 400 })
+	}
 
-  const count = await getViews(env.kv, slug)
-  let result = 1
+	const count = await getViews(env.kv, slug)
+	let result = 1
 
-  if (count !== null) {
-    result = count + 1
-  }
+	if (count !== null) {
+		result = count + 1
+	}
 
-  setViews(env.kv, slug, result)
+	setViews(env.kv, slug, result)
 
-  return new Response(`${result}`, { status: 200 })
+	return new Response(`${result}`, { status: 200 })
 })
 
-router.all('*', (req) => new Response(`API ${req.method} ${new URL(req.url).pathname} | ${new Date()}`, { status: 418 }))
+router.all(
+	'*',
+	(req) =>
+		new Response(`API ${req.method} ${new URL(req.url).pathname} | ${new Date()}`, { status: 418 })
+)
